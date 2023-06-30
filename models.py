b58c8252975a3197ea36ca3b00e275511106809f
a22luisap <a22luisap@iessanclemente.net>
2023-06-30 22:51:01 +0200

    correccion-paginacion

--- models.py
+++ models.py
@@ -6,85 +6,85 @@ class CSVModel:
         self.ruta_archivo_datos = os.path.join(os.path.dirname(__file__), 'data', 'datos.csv')
         self.codificacion_csv = 'utf-8-sig'
 
+    def _abrir_archivo(self, modo):
+        return open(self.ruta_archivo_datos, modo, newline='', encoding=self.codificacion_csv)
+
+    def _obtener_lector_csv(self):
+        archivo = self._abrir_archivo('r')
+        lector = csv.DictReader(archivo)
+        return archivo, lector
+
+    def _obtener_escritor_csv(self, nombres_campos):
+        archivo = self._abrir_archivo('w')
+        escritor = csv.DictWriter(archivo, fieldnames=nombres_campos)
+        escritor.writeheader()
+        return archivo, escritor
+
+    def _obtener_todas_las_filas(self):
+        archivo, lector = self._obtener_lector_csv()
+        filas = list(lector)
+        archivo.close()
+        return filas
+
+    def _guardar_filas(self, filas):
+        archivo, escritor = self._obtener_escritor_csv(filas[0].keys())
+        escritor.writerows(filas)
+        archivo.close()
+
     def obtener_todos_los_datos(self):
         try:
-            with open(self.ruta_archivo_datos, 'r', encoding=self.codificacion_csv) as archivo:
-                lector = csv.DictReader(archivo)
-                return list(lector)
+            _, lector = self._obtener_lector_csv()
+            return list(lector)
         except FileNotFoundError:
             return []
 
     def obtener_datos(self, id):
         try:
-            with open(self.ruta_archivo_datos, 'r', encoding=self.codificacion_csv) as archivo:
-                lector = csv.DictReader(archivo)
-                for fila in lector:
-                    if fila['id'] == id:
-                        return fila
+            _, lector = self._obtener_lector_csv()
+            for fila in lector:
+                if fila['id'] == id:
+                    return fila
         except FileNotFoundError:
             pass
         return {}
 
     def agregar_datos(self, datos):
+        filas = self._obtener_todas_las_filas()
         ultimo_id = self.obtener_ultimo_id()
         nuevo_id = ultimo_id + 1
         datos['id'] = str(nuevo_id)
-
-        nombres_campos = ['id'] + list(datos.keys())
-
-        try:
-            with open(self.ruta_archivo_datos, 'a', newline='', encoding=self.codificacion_csv) as archivo:
-                escritor = csv.DictWriter(archivo, fieldnames=nombres_campos)
-                if archivo.tell() == 0:
-                    escritor.writeheader()
-                escritor.writerow(datos)
-        except FileNotFoundError:
-            pass
+        filas.append(datos)
+        self._guardar_filas(filas)
 
     def actualizar_datos(self, id, datos_actualizados):
         filas = []
         try:
-            with open(self.ruta_archivo_datos, 'r', encoding=self.codificacion_csv) as archivo:
-                lector = csv.DictReader(archivo)
-                nombres_campos = lector.fieldnames
-                for fila in lector:
-                    if fila['id'] == id:
-                        filas.append(datos_actualizados)
-                    else:
-                        filas.append(fila)
+            for fila in self._obtener_todas_las_filas():
+                if fila['id'] == id:
+                    filas.append(datos_actualizados)
+                else:
+                    filas.append(fila)
             if not filas:
-                filas.append(nombres_campos)
-            with open(self.ruta_archivo_datos, 'w', newline='', encoding=self.codificacion_csv) as archivo:
-                escritor = csv.DictWriter(archivo, fieldnames=nombres_campos)
-                escritor.writeheader()
-                escritor.writerows(filas)
+                filas.append(self._obtener_lector_csv().fieldnames)
+            self._guardar_filas(filas)
         except FileNotFoundError:
             pass
 
     def eliminar_datos(self, id):
         filas = []
         try:
-            with open(self.ruta_archivo_datos, 'r', encoding=self.codificacion_csv) as archivo:
-                lector = csv.DictReader(archivo)
-                nombres_campos = lector.fieldnames
-                for fila in lector:
-                    if fila['id'] != id:
-                        filas.append(fila)
-            with open(self.ruta_archivo_datos, 'w', newline='', encoding=self.codificacion_csv) as archivo:
-                escritor = csv.DictWriter(archivo, fieldnames=nombres_campos)
-                if filas:
-                    escritor.writeheader()
-                    escritor.writerows(filas)
+            for fila in self._obtener_todas_las_filas():
+                if fila['id'] != id:
+                    filas.append(fila)
+            self._guardar_filas(filas)
         except FileNotFoundError:
             pass
 
     def obtener_ultimo_id(self):
         try:
-            with open(self.ruta_archivo_datos, 'r', encoding=self.codificacion_csv) as archivo:
-                lector = csv.DictReader(archivo)
-                ultimo_id = 0
-                for fila in lector:
-                    ultimo_id = int(fila['id'])
-                return ultimo_id
+            ultimo_id = 0
+            for fila in self._obtener_lector_csv():
+                ultimo_id = int(fila['id'])
+            return ultimo_id
         except FileNotFoundError:
             return 0
